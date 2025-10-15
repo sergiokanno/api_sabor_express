729bd3f796573db1f4583f26880d5cc6759d8d94
Windows (Sergio Kanno) <sergio.kanno@outlook.com.br>
2025-10-15 05:52:13 -0300

    Aula 04 - Requisições, JSON e arquivos

--- app.py
+++ app.py
@@ -1,24 +1,30 @@
-from modelos.restaurante import Restaurante
-from modelos.cardapio.bebida import Bebida
-from modelos.cardapio.prato import Prato
-from modelos.cardapio.sobremesa import Sobremesa
+import requests
+import json
 
-restaurante_praca = Restaurante('praça', 'Gourmet')
-bebida_suco = Bebida('Suco de Melancia', 5.0,'grande')
-bebida_suco.aplicar_desconto()
-prato_paozinho = Prato('Paozinho',2.00,'O melhor pão da cidade')
-prato_paozinho.aplicar_desconto()
-restaurante_praca.adicionar_no_cardapio(bebida_suco)
-restaurante_praca.adicionar_no_cardapio(prato_paozinho)
+url = 'https://guilhermeonrails.github.io/api-restaurantes/restaurantes.json'
+response = requests.get(url)
+print(response)
 
-napolitano = Sobremesa('Napolitano', 32.0, 'Sorvete', 'Pote Grande', 'Sorvete Napolitano')
-restaurante_praca.adicionar_no_cardapio(napolitano)
-print(f'{'Nome'.ljust(25)} | {'Tipo'.ljust(25)} | {'Tamanho'.ljust(25)} | {'Descricao'.ljust(25)} | {'Preço'}')
-print(napolitano)
-print()
+if response.status_code == 200:
+    dados_json = response.json()
+    dados_restaurante = {}
+    for item in dados_json:
+        nome_do_restaurante = item['Company']
+        if nome_do_restaurante not in dados_restaurante:
+            dados_restaurante[nome_do_restaurante] = []
+        
+        dados_restaurante[nome_do_restaurante].append({
+            "item": item['Item'],
+            "price": item['price'],
+            "description": item['description']
+        })
 
-def main():
-    restaurante_praca.exibir_cardapio
 
-if __name__ == '__main__':
-    main()
+else: 
+    print(f'O erro foi {response.status_code}')
+
+for nome_do_restaurante, dados in dados_restaurante.items():
+    nome_do_arquivo = f'{nome_do_restaurante}.json'
+    with open(nome_do_arquivo,'w') as arquivo_restaurante:
+        json.dump(dados,arquivo_restaurante,indent=4)
+
